709b0001c0b234300b9b791418d306edbaf75292
U277717 <U277717@ACC-7387P13.oshkoshglobal.com>
2021-04-07 13:23:51 -0400

    Remved require packages from setup.py

--- TrilabytePyML/setup.py
+++ TrilabytePyML/setup.py
@@ -9,14 +9,15 @@ setup(name='TrilabytePyML',
       license='GPLv3',
       packages=['TrilabytePyML'],
       install_requires=[
-          'pmdarima',
-          'loess',
-          'scikit-learn',
-          'numpy',
-          'scipy',
-          'pandas',
-          'ephem',
-          'pystan',
-          'fbprophet'
       ],
       zip_safe=False)
+
+#           'pmdarima',
+#           'loess',
+#           'scikit-learn',
+#           'numpy',
+#           'scipy',
+#           'pandas',
+#           'ephem',
+#           'pystan',
+#           'fbprophet'
