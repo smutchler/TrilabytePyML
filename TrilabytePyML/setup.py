a8ab2acb3441a7778e00d3b2bc02650092a68f5d
Owner <Owner@10.0.0.93>
2022-08-18 12:04:22 -0400

    v1.8

--- TrilabytePyML/setup.py
+++ TrilabytePyML/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='TrilabytePyML',
-      version='1.7',
+      version='1.8',
       description='Trilabyte Python Machine Learning',
       url='http://github.com/smutchler/src',
       author='Scott Mutchler',
