4d4d63b9daff7d73ae081f0a9dd267fbff53c3ec
smutc <smutc@gram.attlocal.net>
2023-09-12 14:31:10 -0400

    v3.7

--- trilabytepyml/setup.py
+++ trilabytepyml/setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='trilabytepyml',
-      version='3.6',
+      version='3.7',
       description='Trilabyte Python Machine Learning',
       url='http://github.com/smutchler/src',
       author='Scott Mutchler',
